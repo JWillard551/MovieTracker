ba7a947bd6270bfb2ab2a454da7ca68dc359d9a6
Joe Willard <joseph.ryan.willard@gmail.com>
2021-02-12 10:45:21 -0500

    Initial Commit

---
+++ MovieTracker.App/MovieTracker.App/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MovieTracker.App.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace MovieTracker.App.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
