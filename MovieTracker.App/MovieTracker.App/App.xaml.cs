f43d38050dd234f17bdc8b4b3ecd6b29e9bfa80f
Joe Willard <joseph.ryan.willard@gmail.com>
2021-03-29 14:47:27 -0400

    General/SmallDeviceStyles implementation

--- MovieTracker.App/MovieTracker.App/App.xaml.cs
+++ MovieTracker.App/MovieTracker.App/App.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using MovieTracker.App.Services;
 using MovieTracker.App.Views;
 using System;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -8,13 +9,30 @@ namespace MovieTracker.App
 {
     public partial class App : Application
     {
+        const int SMALLWIDTHRESOLUTION = 768;
+        const int SMALLHEIGHTRESOLUTION = 1280;
 
         public App()
         {
             InitializeComponent();
+            LoadDeviceStyles();
 
             DependencyService.Register<MockDataStore>();
             MainPage = new AppShell();
+        }
+
+        public static bool IsSmallDevice()
+        {
+            return (DeviceDisplay.MainDisplayInfo.Width <= SMALLWIDTHRESOLUTION 
+                && DeviceDisplay.MainDisplayInfo.Height <= SMALLHEIGHTRESOLUTION);
+        }
+
+        private void LoadDeviceStyles()
+        {
+            if (IsSmallDevice())
+                primaryDictionary.MergedDictionaries.Add(SmallDeviceStyle.SharedInstance);
+            else
+                primaryDictionary.MergedDictionaries.Add(GeneralDevicesStyle.SharedInstance);
         }
 
         protected override void OnStart()
