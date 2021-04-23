2c7583caceea9665cea4b86c31b22ab8b4ef3209
Joe Willard <joseph.ryan.willard@gmail.com>
2021-04-23 15:20:37 -0400

    IsVisibile added to TagLine label. Move title to tabbedpage titleview. New string converter.

---
+++ MovieTracker.App/MovieTracker.App/Converters/StringIsNullVisibilityCheckConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace MovieTracker.App.Converters
+{
+    public class StringIsNullVisibilityCheckConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null || string.IsNullOrWhiteSpace(value.ToString()))
+                return false;
+            return true;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
