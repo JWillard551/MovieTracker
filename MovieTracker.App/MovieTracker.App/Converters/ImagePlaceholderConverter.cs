1bf3de052e19f19d2c38ef063839155e7dc6e2b6
Joe Willard <joseph.ryan.willard@gmail.com>
2021-04-27 16:20:49 -0400

    New Image placeholder for loading/errors.

---
+++ MovieTracker.App/MovieTracker.App/Converters/ImagePlaceholderConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace MovieTracker.App.Converters
+{
+    public class ImagePlaceholderConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return "imgplaceholder.png";
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
